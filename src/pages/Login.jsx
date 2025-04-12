14dc0435ac4606c4e911c9bd266ed7544b9a7ee1
ShaikAyub7 <skayub929@gmail.com>
2025-04-12 23:24:37 +0530

    /

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,7 +14,6 @@ const Login = () => {
     e.preventDefault();
     try {
       await login({ email, password });
-      toast.success("Login successful!");
       setEmail("");
       setPassword("");
       navigation("/");
