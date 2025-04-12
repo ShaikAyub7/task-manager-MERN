ea1d4ee2fbecaf5593b372ffbd62adf5fdaa5e7f
ShaikAyub7 <skayub929@gmail.com>
2025-04-12 13:46:59 +0530

    createTask component comment delete

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -4,7 +4,6 @@ import Modal from "./MOdal";
 const CreateTask = () => {
   return (
     <div className="p-4 max-w-4xl m-auto ">
-      {/* <Modal name={"New Task"} component={<CreateTask />} /> */}
       <CreateTaskForm />
     </div>
   );
