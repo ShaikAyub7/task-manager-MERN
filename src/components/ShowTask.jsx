49f0125d072a920f3fcb7f2dd6af9b6a45b8ebf8
ShaikAyub7 <skayub929@gmail.com>
2025-04-12 20:23:36 +0530

    .

--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -50,7 +50,7 @@ const ShowTask = () => {
         <div className="grid grid-cols-1">
           {data ? (
             <>
-              <div className=" p-4 mb-5">{tabs[activeTab].content}</div>
+              <div className=" p-4 mb-8">{tabs[activeTab].content}</div>
             </>
           ) : (
             <>
